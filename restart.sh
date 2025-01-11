d9b82445f23a93c4142a86860679dcc73f397659
eins0fx <dennyapriliopratama@gmail.com>
2025-01-11 16:25:27 +0700

    added restart and stop bash script

---
+++ restart.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Function to print step information
+print_step() {
+    echo "===================================="
+    echo "Step: $1"
+    echo "===================================="
+}
+
+# Stop all containers and remove compose services
+print_step "Stopping Docker Compose services"
+docker compose down
+
+# Stop all running containers
+print_step "Stopping all running containers"
+docker stop $(docker ps -aq)
+
+# Remove all containers
+print_step "Removing all containers"
+docker rm $(docker ps -aq)
+
+# Remove all images forcefully
+print_step "Removing all Docker images"
+docker rmi -f $(docker images -aq)
+
+# Remove all volumes
+print_step "Removing all Docker volumes"
+docker volume ls -q | xargs -r docker volume rm
+
+# Start compose services in detached mode
+print_step "Starting Docker Compose services"
+docker compose up -d
+
+# Show logs
+print_step "Showing Docker Compose logs"
+docker compose logs -f
